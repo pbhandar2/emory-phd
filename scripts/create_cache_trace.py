d9927edafa2cda07891ba7d72b3d97580de207ca
Pranav Bhandari <pranav@mjolnir.mathcs.emory.edu>
2023-08-28 21:42:46 -0400

    update create cache trace

--- scripts/create_cache_trace.py
+++ scripts/create_cache_trace.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
 
     parser.add_argument("workload_name", type=str, help="Name of the workload.")
 
-    parser.add_argument("stack_binary_path", type-str, help="Path to binary that computes stack distance.")
+    parser.add_argument("stack_binary_path", type=str, help="Path to binary that computes stack distance.")
 
     parser.add_argument("--workload_type", default="cp", type=str, help="Workload type.")
 
