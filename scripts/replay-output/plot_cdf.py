ef2da0db0eb2bea62dd80db033227fd75e6c8245
Pranav Bhandari <pranav@mjolnir.mathcs.emory.edu>
2024-03-19 17:42:12 -0400

    increase size of marker in cdf plot

--- scripts/replay-output/plot_cdf.py
+++ scripts/replay-output/plot_cdf.py
@@ -22,28 +22,28 @@ def main():
 
     # calculate the proportional values of samples
     p = 1. * arange(len(set1_df)) / (len(set1_df) - 1)
-    ax.plot(p, set1_df, '-o', label="20%", markersize=20, alpha=0.6)
+    ax.plot(p, set1_df, '-o', label="20%", markersize=30, alpha=0.6)
 
     # 10_4_42 for w96 no preprocess
     set2_df = sorted(err_df[(err_df["rate"]==10) & (err_df["bits"]==4) & (err_df["pp_n"] == -1)]["bandwidth"])
 
     # calculate the proportional values of samples
     p = 1. * arange(len(set2_df)) / (len(set2_df) - 1)
-    ax.plot(p, set2_df, '-*', label="10%", markersize=20, alpha=0.6)
+    ax.plot(p, set2_df, '-*', label="10%", markersize=30, alpha=0.6)
 
     # 20 process from no preprocess
     set3_df = sorted(err_df[(err_df["rate"]==10) & (err_df["bits"]==4) & (err_df["pp_n"] > 0)]["bandwidth"])
 
     # calculate the proportional values of samples
     p = 1. * arange(len(set3_df)) / (len(set3_df) - 1)
-    ax.plot(p, set3_df, '-^', label="10% + PP", markersize=20, alpha=0.6)
+    ax.plot(p, set3_df, '-^', label="10% + PP", markersize=30, alpha=0.6)
 
     # 20 process from no preprocess
     set4_df = sorted(err_df[(err_df["rate"]==40) & (err_df["bits"]==4) & (err_df["pp_n"] == -1)]["bandwidth"])
 
     # calculate the proportional values of samples
     p = 1. * arange(len(set4_df)) / (len(set4_df) - 1)
-    ax.plot(p, set4_df, '-d', label="40%", markersize=20, alpha=0.6)
+    ax.plot(p, set4_df, '-d', label="40%", markersize=30, alpha=0.6)
 
     ax.set_ylabel("Percent Bandwidth Error (%)", fontsize=40)
     ax.set_xlabel("CDF", fontsize=40)
